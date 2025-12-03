ccedf675107bbe7b20d6bd726460fbc6cd0516cf
wulu <wulu90@outlook.com>
2025-12-03 14:29:27 +0800

    03 p2

--- 03.cpp
+++ 03.cpp
@@ -37,6 +37,36 @@ void part1() {
     println("{}", res);
 }
 
+pair<char, size_t> find_largest_joltage_with_place_remain(const string& bank, size_t place, size_t start) {
+    auto digit = bank[start];
+    size_t inx = start;
+    for (auto i = start + 1; i <= bank.size() - place; ++i) {
+        if (bank[i] > digit) {
+            digit = bank[i];
+            inx   = i;
+        }
+    }
+    return {digit, inx};
+}
+
+void part2() {
+    fstream input{"input/03"};
+
+    int64_t res = 0;
+    for (string line; getline(input, line);) {
+        auto [c, n]     = find_largest_joltage_with_place_remain(line, 12, 0);
+        int64_t largest = c - '0';
+        for (size_t i = 11; i >= 1; --i) {
+            auto [ci, ni] = find_largest_joltage_with_place_remain(line, i, n + 1);
+            n             = ni;
+            largest       = largest * 10 + (ci - '0');
+        }
+        res += largest;
+    }
+    println("{}", res);
+}
+
 int main() {
     part1();
+    part2();
 }
