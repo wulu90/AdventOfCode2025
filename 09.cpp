ba158c73e01c0b09c6d23c151f50d972c5b236bc
wulu <wulu90@outlook.com>
2025-12-09 16:22:41 +0800

    09 p2

--- 09.cpp
+++ 09.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <print>
+#include <ranges>
 #include <vector>
 
 using namespace std;
@@ -20,20 +21,39 @@ void part1() {
     int64_t largest_rectangle_area = 0;
     for (size_t i = 0; i < red_tiles.size() - 1; ++i) {
         for (size_t j = i + 1; j < red_tiles.size(); ++j) {
-            auto [xi, yi] = red_tiles[i];
-            auto [xj, yj] = red_tiles[j];
-            if (xi == xj && yi != yj) {
-                largest_rectangle_area = max(largest_rectangle_area, abs(yi - yj) + 1);
-            } else if (xi != xj && yi == yj) {
-                largest_rectangle_area = max(largest_rectangle_area, abs(xi - xj) + 1);
-
-            } else {
-                largest_rectangle_area = max(largest_rectangle_area, (abs(xi - xj) + 1) * (abs(yi - yj) + 1));
-            }
+            auto [xi, yi]          = red_tiles[i];
+            auto [xj, yj]          = red_tiles[j];
+            largest_rectangle_area = max(largest_rectangle_area, (abs(xi - xj) + 1) * (abs(yi - yj) + 1));
         }
     }
 
     println("{}", largest_rectangle_area);
+
+    vector<pair<int64_t, int64_t>> first_vertex{red_tiles[0]};
+    int64_t largest_rg_rectangle_area = 0;
+    for (size_t i = 0; i < red_tiles.size() - 1; ++i) {
+        for (size_t j = i + 1; j < red_tiles.size(); ++j) {
+            auto [rxmin, rxmax] = minmax(red_tiles[i].first, red_tiles[j].first);
+            auto [rymin, rymax] = minmax(red_tiles[i].second, red_tiles[j].second);
+
+            // green vertex
+            bool green_line_cross = false;
+            for (auto [v1, v2] : ranges::views::concat(red_tiles, first_vertex) | ranges::views::adjacent<2>) {
+                auto [gxmin, gxmax] = minmax(v1.first, v2.first);
+                auto [gymin, gymax] = minmax(v1.second, v2.second);
+
+                if (rxmin < gxmax && rxmax > gxmin && rymin < gymax && rymax > gymin) {
+                    green_line_cross = true;
+                    break;
+                }
+            }
+
+            if (!green_line_cross) {
+                largest_rg_rectangle_area = max(largest_rg_rectangle_area, (rxmax - rxmin + 1) * (rymax - rymin + 1));
+            }
+        }
+    }
+    println("{}", largest_rg_rectangle_area);
 }
 
 int main() {
